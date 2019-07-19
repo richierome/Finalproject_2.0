736dd3b2d7796ae8d47fb3b9a327172bc2073657
richie <rickromero99@gmail.com>
2019-07-18 19:47:32 -0700

    Updates

---
+++ precache-manifest.7c8357e820083112cb22fc998b735635.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45dff17654183b3c82dd08cf392bf87b",
+    "url": "/Finalproject_2.0/index.html"
+  },
+  {
+    "revision": "cd408abc9f7323b36048",
+    "url": "/Finalproject_2.0/static/css/main.fe564851.chunk.css"
+  },
+  {
+    "revision": "0db0a3ffb355978d1d59",
+    "url": "/Finalproject_2.0/static/js/2.eba2cae6.chunk.js"
+  },
+  {
+    "revision": "cd408abc9f7323b36048",
+    "url": "/Finalproject_2.0/static/js/main.4e9a3b26.chunk.js"
+  },
+  {
+    "revision": "42439a060ec38b065d49",
+    "url": "/Finalproject_2.0/static/js/runtime~main.f257dcd9.js"
+  },
+  {
+    "revision": "e90f8a9f040662ac088e36f8067be9bd",
+    "url": "/Finalproject_2.0/static/media/Large-Fireball.e90f8a9f.mp3"
+  },
+  {
+    "revision": "67355f826c5edd1f03c4f9f7bfb09b72",
+    "url": "/Finalproject_2.0/static/media/LogoMakr.67355f82.png"
+  },
+  {
+    "revision": "5befb05dc8e18cdfabb15cea2c088f5d",
+    "url": "/Finalproject_2.0/static/media/a.5befb05d.mp3"
+  },
+  {
+    "revision": "3e4cb9a1e300a8972af38f6fb4ad3fd2",
+    "url": "/Finalproject_2.0/static/media/alien.3e4cb9a1.png"
+  },
+  {
+    "revision": "3247bdf74c71def6f2becb42133db655",
+    "url": "/Finalproject_2.0/static/media/astro4.3247bdf7.png"
+  },
+  {
+    "revision": "266c1f232e29fe57d16a54058e481adb",
+    "url": "/Finalproject_2.0/static/media/b.266c1f23.mp3"
+  },
+  {
+    "revision": "a14bd2a0ba76610d070927715bbd4c65",
+    "url": "/Finalproject_2.0/static/media/c.a14bd2a0.mp3"
+  },
+  {
+    "revision": "63ce6765ee0ede23402231b9d25cae7a",
+    "url": "/Finalproject_2.0/static/media/d.63ce6765.mp3"
+  },
+  {
+    "revision": "20fb76ce19b9fda7eb9ef6404d584671",
+    "url": "/Finalproject_2.0/static/media/e.20fb76ce.wav"
+  },
+  {
+    "revision": "88e309aee4cea37afb108689614800e7",
+    "url": "/Finalproject_2.0/static/media/f.88e309ae.mp3"
+  },
+  {
+    "revision": "1e6115e85f53bf040d6b458d76f5bc1a",
+    "url": "/Finalproject_2.0/static/media/g.1e6115e8.mp3"
+  },
+  {
+    "revision": "4111dc95d82c77addfd9d8d799e14f13",
+    "url": "/Finalproject_2.0/static/media/h.4111dc95.mp3"
+  },
+  {
+    "revision": "1e47cdd57172a3055d130959c147f494",
+    "url": "/Finalproject_2.0/static/media/i.1e47cdd5.mp3"
+  },
+  {
+    "revision": "160d540355e1fec99734053529d76f01",
+    "url": "/Finalproject_2.0/static/media/j.160d5403.mp3"
+  },
+  {
+    "revision": "31fb795c17f0784294e78e616ea5695a",
+    "url": "/Finalproject_2.0/static/media/k.31fb795c.mp3"
+  },
+  {
+    "revision": "7755c2c4f5ad734214b5ee7c472ff215",
+    "url": "/Finalproject_2.0/static/media/l.7755c2c4.mp3"
+  },
+  {
+    "revision": "551bebcf2ab70ed9570a761999983a1c",
+    "url": "/Finalproject_2.0/static/media/m.551bebcf.mp3"
+  },
+  {
+    "revision": "c61188392d5fe28d9a2358a9342066db",
+    "url": "/Finalproject_2.0/static/media/n.c6118839.mp3"
+  },
+  {
+    "revision": "e9bde2dec2c4d576ffd9b9714a8a391d",
+    "url": "/Finalproject_2.0/static/media/o.e9bde2de.mp3"
+  },
+  {
+    "revision": "3bb3a5d626dfc3be9a3e3ff1e39874ef",
+    "url": "/Finalproject_2.0/static/media/p.3bb3a5d6.mp3"
+  },
+  {
+    "revision": "3c0b30ebb8b4c59d9b251b34cf02b3e0",
+    "url": "/Finalproject_2.0/static/media/q.3c0b30eb.mp3"
+  },
+  {
+    "revision": "812115d2e8d20a0c6b686b24700c3cff",
+    "url": "/Finalproject_2.0/static/media/r.812115d2.mp3"
+  },
+  {
+    "revision": "f003a79e6d93e7dd2d1badf2fbed6249",
+    "url": "/Finalproject_2.0/static/media/s.f003a79e.mp3"
+  },
+  {
+    "revision": "f312001529dcf1d329a969641b1d000c",
+    "url": "/Finalproject_2.0/static/media/space3.f3120015.jpeg"
+  },
+  {
+    "revision": "0f9b606862591dd603c44361b766695f",
+    "url": "/Finalproject_2.0/static/media/star2.0f9b6068.jpg"
+  },
+  {
+    "revision": "97bc2416ff706d1d5addf2c174028364",
+    "url": "/Finalproject_2.0/static/media/t.97bc2416.mp3"
+  },
+  {
+    "revision": "cddb2021d4c3a22685d7c8d8b0f409c1",
+    "url": "/Finalproject_2.0/static/media/typinggame.cddb2021.png"
+  },
+  {
+    "revision": "fca92dbcb1db7625a3e9ac69147ea54b",
+    "url": "/Finalproject_2.0/static/media/u.fca92dbc.mp3"
+  },
+  {
+    "revision": "08ee15af31dba0c2d7708954f6fa4cf5",
+    "url": "/Finalproject_2.0/static/media/v.08ee15af.mp3"
+  },
+  {
+    "revision": "ad6f6f3802979b739b7b9d13a9fe395f",
+    "url": "/Finalproject_2.0/static/media/w.ad6f6f38.mp3"
+  },
+  {
+    "revision": "4aac28d071cbcdde0d4f31f37c2fa5c8",
+    "url": "/Finalproject_2.0/static/media/web-background.4aac28d0.jpg"
+  },
+  {
+    "revision": "95768f1ae555fa08ed8bde454650aa98",
+    "url": "/Finalproject_2.0/static/media/x.95768f1a.mp3"
+  },
+  {
+    "revision": "52b7bcf4fbe7f204de31eabb70690843",
+    "url": "/Finalproject_2.0/static/media/y.52b7bcf4.mp3"
+  },
+  {
+    "revision": "f88d1338946476e9bb294a85ecda3a07",
+    "url": "/Finalproject_2.0/static/media/z.f88d1338.mp3"
+  }
+]);
